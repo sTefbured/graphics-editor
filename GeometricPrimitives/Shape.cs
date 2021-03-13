fbecfcbf09cf17d4ed6b7143815d9b6d7c25a535
sTefbured <stef.for.ever2000@gmail.com>
2021-03-13 22:59:32 +0300

    Add geometric primitives base library. Add Line library.

---
+++ GeometricPrimitives/Shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Drawing;
+
+namespace GeometricPrimitives
+{
+    [Serializable]
+    public abstract class Shape
+    {
+        public abstract void Draw(Graphics graphics, Pen pen);
+    }
+}
---
+++ Line/Line.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using GeometricPrimitives;
+
+namespace Line
+{
+    [Serializable]
+    public class Line : Shape
+    {
+        public Point First { get; }
+        public Point Second { get; }
+
+        public Line(Point first, Point second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public override void Draw(Graphics graphics, Pen pen)
+        {
+            graphics.DrawLine(pen, First, Second);
+        }
+    }
+}
